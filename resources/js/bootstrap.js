b4f7a0dc43e351a7a0deb2e0b437dc84b5133e46
Ohidul Islam <ohidul.islam951@gmail.com>
2024-12-24 22:59:58 +0600

    Handle invalid token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,14 @@ const token = localStorage.getItem('barn2AppToken');
 
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+} else {
+    const host = new URLSearchParams(location.search).get("host");
+    if ( host && typeof shopify === 'object' ) {
+        // Add authorization token into localstorage to use in API requests
+        shopify.idToken().then((token) => {
+            localStorage.setItem('barn2AppToken', token);
+        });
+    }
 }
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
